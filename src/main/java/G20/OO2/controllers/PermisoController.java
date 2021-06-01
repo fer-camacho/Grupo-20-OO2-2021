670043c65fe706118dc67a5d9b748774053d976f
Fernando Camacho <camachofernandojl@gmail.com>
2021-05-31 22:41:52 -0300

    helper actualizado

--- src/main/java/G20/OO2/controllers/PermisoController.java
+++ src/main/java/G20/OO2/controllers/PermisoController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import G20.OO2.helpers.ViewRouteHelper;
 import G20.OO2.models.LugarModel;
 import G20.OO2.models.PermisoDiarioModel;
 import G20.OO2.models.PermisoPeriodoModel;
@@ -71,7 +72,7 @@ public class PermisoController {
 	
 	@GetMapping("/diario/new")
 	public ModelAndView newDiario() {
-		ModelAndView mAV = new ModelAndView("permiso/add_diario");		
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.ADD_DIARIO);		
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		boolean anonimo = false;
@@ -111,7 +112,7 @@ public class PermisoController {
 	
 	@GetMapping("/periodo/new")
 	public ModelAndView newPeriodo() {
-		ModelAndView mAV = new ModelAndView("permiso/add_periodo");		
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.ADD_PERIODO);		
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		boolean anonimo = false;
@@ -157,7 +158,7 @@ public class PermisoController {
 	
 	@GetMapping("/persona")
 	public ModelAndView permisosPorPersona() {
-		ModelAndView mAV = new ModelAndView("permiso/personas");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PERSONAS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		boolean anonimo = false;
@@ -173,7 +174,7 @@ public class PermisoController {
 	
 	@GetMapping("/persona/{id}")
 	public ModelAndView permisosPorPersona(@PathVariable("id") int id) {
-		ModelAndView mAV = new ModelAndView("permiso/permisos");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PERMISOS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		boolean anonimo = false;
 		if (roleString.equals("[ROLE_ANONYMOUS]")) anonimo=true;
@@ -196,7 +197,7 @@ public class PermisoController {
 	@PreAuthorize("hasRole('ROLE_AUDIT')")
 	@GetMapping("/rodado")
 	public ModelAndView permisosPorRodado() {
-		ModelAndView mAV = new ModelAndView("permiso/rodados");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.RODADOS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		boolean anonimo = false;
@@ -212,7 +213,7 @@ public class PermisoController {
 	
 	@GetMapping("/rodado/{id}")
 	public ModelAndView permisosPorRodado(@PathVariable("id") int id) {
-		ModelAndView mAV = new ModelAndView("permiso/permisos");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PERMISOS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		boolean anonimo = false;
 		if (roleString.equals("[ROLE_ANONYMOUS]")) anonimo=true;
@@ -231,7 +232,7 @@ public class PermisoController {
 	@PreAuthorize("hasRole('ROLE_AUDIT')")
 	@GetMapping("/fechas")
 	public ModelAndView permisosEntreFechas() {
-		ModelAndView mAV = new ModelAndView("permiso/fechas");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.FECHAS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		asignarPerfil(mAV, roleString);
@@ -243,7 +244,7 @@ public class PermisoController {
 	@PostMapping("/fechas/grilla")
 	public ModelAndView permisosEntreFechas(@ModelAttribute("rangoFechas") RangoFechasModel rangoFechas, BindingResult result,
 			RedirectAttributes redirect) throws UnsupportedEncodingException, MessagingException {
-		ModelAndView mAV = new ModelAndView("permiso/permisos");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PERMISOS);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 	
 		List<PermisoDiarioModel> diario = permisoDiarioService.traerPorFecha(rangoFechas.getFechaInicio(), rangoFechas.getFechaFin().plusDays(1));
@@ -258,7 +259,7 @@ public class PermisoController {
 	@PreAuthorize("hasRole('ROLE_AUDIT')")
 	@GetMapping("/fechas/lugar")
 	public ModelAndView permisosEntreFechasYLugar() {
-		ModelAndView mAV = new ModelAndView("permiso/fechas_lugar");
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.FECHAS_LUGAR);
 		String roleString = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
 		
 		asignarPerfil(mAV, roleString);
--- src/main/java/G20/OO2/helpers/ViewRouteHelper.java
+++ src/main/java/G20/OO2/helpers/ViewRouteHelper.java
@@ -2,15 +2,18 @@ package G20.OO2.helpers;
 
 public class ViewRouteHelper {
 	/**** Views ****/
+	
 	//HOME
 	public final static String INDEX = "/home/index";
 	public final static String HELLO = "home/hello";
+	
 	
 	//ROLE
 	public final static String ROLE_ABM = "role/abm_roles";
 	public final static String ROLE_ADD = "role/add_role";
 	public final static String ROLE_EDIT = "role/edit_role";
 	public final static String ROLE_LIST = "role/list_roles";
+	
 	
 	//USER
 	public final static String USER_ABM = "user/abm_users";
@@ -20,17 +23,28 @@ public class ViewRouteHelper {
 	public final static String USER_LOGIN = "user/login";
 	public final static String USER_LOGOUT = "user/logout";
 	
+	
 	//PËRSONA
 	public final static String PERSONA_ADD = "persona/add_persona";
+	
 	
 	//RODADO
 	public final static String RODADO_ADD = "rodado/add_rodado";
 	
-	//PERMISO
-	public final static String PERMISO_ADD= "permiso/add_permiso";
-		
+	
 	//LUGAR
 	public final static String LUGAR_ADD = "lugar/add_lugar";
+	
+	
+	//PERMISO
+	public final static String ADD_DIARIO = "permiso/add_diario";
+	public final static String ADD_PERIODO = "permiso/add_periodo";
+	public final static String FECHAS_LUGAR = "permiso/fechas_lugar";
+	public final static String FECHAS = "permiso/fechas";
+	public final static String PERMISOS = "permiso/permisos";
+	public final static String PERSONAS = "permiso/personas";
+	public final static String RODADOS = "permiso/rodados";
+	
 	
 	/**** Redirects ****/
 	public final static String ROUTE = "/index";
